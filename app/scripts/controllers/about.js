55bb334ab86426148c812fec318773398250b98b
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-02-23 17:44:18 +0000

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medtechClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the medtechClientApp
+ */
+angular.module('medtechClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
