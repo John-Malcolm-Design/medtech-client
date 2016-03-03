c468941b2e0253a682f36a91b4aac0b1c1c77ae7
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2016-03-03 11:45:23 +0000

    jh demo

---
+++ app/scripts/controllers/faq.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name medtechClientApp.controller:FaqCtrl
+ * @description
+ * # FaqCtrl
+ * Controller of the medtechClientApp
+ */
+angular.module('medtechClientApp')
+  .controller('FaqCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the medtechClientApp
  */
 angular.module('medtechClientApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', ['$scope', '$filter', function ($scope, $filter) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    
+    $scope.name = 'John Malcolm';
+    
+    $scope.handle='';
+    
+    $scope.characters = 5;
+    
+    // Two way data binding with directives example
+    // n.b - Digest Loop and Watch list
+    $scope.lowerCaseHandle = function(){
+        return $filter('lowercase')($scope.handle);
+    };
+    
+    // $scope.apply(function(){}) 
+    // ^ Tells Angular to run the Digest Loop
+    // AngularJS auto wraps everything in scope.apply if it
+    // is contained within the AngularJS architecture
+    $scope.$watch('handle', function(newValue, oldValue){
+        console.info('Changed');
+        console.log('Old: ' + oldValue);
+        console.log('New: ' + newValue);
+    });
+    
+  }]);
